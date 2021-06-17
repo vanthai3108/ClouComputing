85c369660d905a7ace5aa471e31d366428254bb6
Thai <vanthai3108@gmail.com>
2021-06-17 13:53:48 +0700

    thai

--- include/connect.php
+++ include/connect.php
@@ -1,14 +1,14 @@
 <?php
+$connect = mysqli_connect('localhost','root','','mydatabase'); 
 
-// $server = "35.172.43.136";
-$server = "18.204.204.99";
-$user = "thai";
-$pass = "thai";
-$database = "cloud";
-$connect = mysqli_connect($server, $user, $pass, $database); 
-if (!$connect) {
-	die("Connect Failed:".mysqli_connect_error());
-	# code...
-}
-mysqli_set_charset($connect, 'UTF8');
+// $server = "18.204.204.99";
+// $user = "thai";
+// $pass = "thai";
+// $database = "cloud";
+// $connect = mysqli_connect($server, $user, $pass, $database); 
+// if (!$connect) {
+// 	die("Connect Failed:".mysqli_connect_error());
+// 	# code...
+// }
+// mysqli_set_charset($connect, 'UTF8');
 ?>
